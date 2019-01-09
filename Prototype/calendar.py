a78f884d7829dd540bbf174e36bcef5adc074c6d
18385963 <2013tim.g@gmail.com>
2019-01-08 23:11:54 -0500

    WIP commit

--- Prototype/calendar.py
+++ Prototype/calendar.py
@@ -41,7 +41,28 @@ def main():
         duration_mins = divmod(duration.total_seconds(),60)
         duration_mins = duration_mins[0] + duration_mins[1]
         print(duration_mins, event['summary'])
+        
+def freetimecheck():
+    service = connection.googleCalendar(SCOPES)
     
-    print(event)
+    # Call the Calendar API
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    print('Getting the upcoming 10 events')
+    events_result = service.events().list(calendarId='t4p9h18kn9ka3nf8sf6teobfbfouoo6t@import.calendar.google.com',
+                                        timeMin=now,
+                                        maxResults=10, singleEvents=True,
+                                        orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        print('No upcoming events found.')
+    for event in events:
+        start_time, end_time = calhelp.getEventStartEnd(event)
+        duration = end_time - start_time
+        duration_mins = divmod(duration.total_seconds(),60)
+        duration_mins = duration_mins[0] + duration_mins[1]
+        print(duration_mins, event['summary'])
+        
+        
 if __name__ == '__main__':
-    main()
+    freetimecheck()
--- Prototype/calendarHelperFun.py
+++ Prototype/calendarHelperFun.py
@@ -15,3 +15,35 @@ def str2time(string):
         dt = datetime.datetime.strptime(string,timeformat)
     
     return dt
+
+def minsLeftinWeek():
+    now = datetime.datetime.now()
+    day = now.weekday()
+    week_remain_days = 6 - day
+    
+    day_remain_mins = datetime.datetime.fromordinal(
+                        datetime.date.today().toordinal())
+    day_remain_mins = now - day_remain_mins
+    day_remain_mins = divmod(day_remain_mins.total_seconds(),60)
+    day_remain_mins = 24*60 - (day_remain_mins[0] + day_remain_mins[1])
+    
+    total_remain_mins = week_remain_days * 24 * 60 + day_remain_mins
+
+    print('You have ' + str(total_remain_mins) + ' minutes left this week!')
+    
+minsLeftinWeek()
+
+def getEventStartEnd(event):
+    '''returns start time & end time as datetime objects from an event struct'''
+    start_string = event['start'].get('dateTime',event['start'].get('date'))
+    start_time = str2time(start_string)
+    
+    end_string = event['end'].get('dateTime',0)
+    if not end_string:
+        end_time = start_time + datetime.timedelta(days = 1)
+    else:
+        end_time = str2time(end_string)
+        
+    return start_time,end_time
+
+
